4379479a0b045a5e65678fb137a96038e53ec98c
lenz <lenz@mac>
2019-09-29 14:43:59 +0200

    Not working

--- src/commonMain/kotlin/knativedemo/Main.kt
+++ src/commonMain/kotlin/knativedemo/Main.kt
@@ -1,7 +1,16 @@
 package l3nz
 
+import io.ktor.client.HttpClient
+
+
 fun hello(): String = "Hello, Kotlin/Native! It pretty much rocks!"
 
 fun main() {
     println(hello())
+
+    val c = HttpClient()
+
+    println( "HTTP client created" )
+
+
 }
